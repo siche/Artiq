9f67c229d3170526552b4778f3b8d76567e62061
siche <sichecmm@gmail.com>
2021-02-24 00:30:41 +0800

    auto update

--- Sequence/435_AOM_frequency_scan.py
+++ Sequence/435_AOM_frequency_scan.py
@@ -165,7 +165,7 @@ class KasliTester(EnvExperiment):
 
         pmt_on()
         init_fre = 237.5
-        lock_point = 871.034667
+        lock_point = 871.034663
         scan_step = 0.001
         rabi_time = 100
         N = 4500
